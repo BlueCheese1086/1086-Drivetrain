860db1e67a25fc8d8ef14f4f181e13e0893f170e
turbojax07 <turbojax07@gmail.com>
2025-04-04 16:48:07 -0400

    Adding an error notification if the attempts failed

--- PhoenixUtil.java
+++ PhoenixUtil.java
@@ -20,9 +20,10 @@ public class PhoenixUtil {
     /** Attempts to run the command until no error is produced. */
     public static void tryUntilOk(int maxAttempts, Supplier<StatusCode> command) {
         for (int i = 0; i < maxAttempts; i++) {
-            var error = command.get();
-            if (error.isOK())
-                break;
+            StatusCode code = command.get();
+            if (code.isOK()) return;
         }
+
+        System.out.println("Failed to get an OK");
     }
 }
