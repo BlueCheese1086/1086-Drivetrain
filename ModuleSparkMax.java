5935c00b37e812554edd5241503c7b06f212a3b1
turbojax07 <turbojax07@gmail.com>
2025-05-04 20:19:17 -0400

    Removing unused imports

--- ModuleSparkMax.java
+++ ModuleSparkMax.java
@@ -19,8 +19,6 @@ import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.units.Measure;
-import edu.wpi.first.units.Unit;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
